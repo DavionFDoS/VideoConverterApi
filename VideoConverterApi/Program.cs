c678342323dfb7aafb90987668895241f441e476
davionfdos <matveydetochenko@gmail.com>
2023-04-12 11:48:22 +0300

    Добавьте файлы проекта.

---
+++ VideoConverterApi/Program.cs
@@ -0,0 +1,23 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapPost("/convertvideo", () =>
+{
+    
+})
+.WithName("ConvertVideo")
+.WithOpenApi();
+
+app.Run();
